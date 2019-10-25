1d75baa3e631dfed6aeb1149a52d5f564024365f
Denis Bekhterev <constairs@gmail.com>
2019-10-25 10:36:55 +0700

    Add test http request to hackernewsApi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+class Item {
+  by: string;
+  depends: string;
+  id: number;
+  kids: number[];
+  score: number;
+  time: number;
+  title: string;
+  type: string;
+  url: string;
+}
 
 @Component({
   selector: 'app',
-  template: `<label>Name: </label>
-              <input [(ngModel)]="name" placeholder="name" />
-              <h1>{{name}}</h1>
+  template: `
+              <div>
+                <p>
+                  <span>{{ item.by }}</span>
+                  <span>{{ getDateString(item.time) }}</span>
+                </p>
+                <p>{{ item.type }}</p>
+                <h2><a [href]="item.url">{{ item.title }}</a></h2>
+              </div>
             `,
 })
-export class AppComponent {
-  name = '';
+export class AppComponent implements OnInit {
+  item: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    const testUrl = 'https://hacker-news.firebaseio.com/v0/item/8863.json?print=pretty';
+    this.http.get(testUrl).subscribe((data) => {
+      console.log(data);
+      this.item = data;
+    })
+  }
+
+  getDateString(time: number): string {
+    const dateObj = new Date(time);
+    const date = dateObj.getUTCDate();
+    const month = dateObj.getUTCMonth() < 10 ? '0' + (dateObj.getUTCMonth() + 1) : dateObj.getUTCMonth() + 1;
+    const year = dateObj.getUTCFullYear();
+    return `${date}.${month}.${year}`;
+  }
 }
